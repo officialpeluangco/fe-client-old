572fa1d41e1679b714dc615dadcd26ba9ace23ca
cihuyyama <miqbalalhabib@gmail.com>
2024-03-26 22:27:14 +0700

    feat: add search bar component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,9 +1,13 @@
-import Header from './Header'
+import SearchBar from "./SearchBar"
+
 
 const HeroSection = () => {
   return (
-    <section className='w-full p-4 h-[500px] bg-gradient-to-b from-indigo-900 to-white mx-auto'>
-        <Header white />
+    <section className='font-inter w-full flex flex-col gap-10 items-center justify-center p-4 h-[600px] bg-gradient-to-b from-indigo-900 to-white mx-auto'>
+      <div className="text-4xl text-white">
+        <span className="font-normal">Mau Sukses? Buka Bisnis </span><span className="font-bold">Makanan</span>
+      </div>
+      <SearchBar />
     </section>
   )
 }
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,27 @@
+import { Search } from "lucide-react"
+
+const SearchBar = () => {
+    return (
+        <div className='font-inter flex flex-row gap-4 bg-slate-200 min-w-[750px] w-fit h-[60px] rounded-full p-[9px]'>
+            <input
+                type="text"
+                placeholder="Kebuli Abuya"
+                className="bg-white max-w-[300px] w-full h-full rounded-full placeholder:text-sm focus:outline-none px-3">
+
+            </input>
+            <div className="hover:bg-slate-100 cursor-pointer w-full max-w-[150px] flex flex-col border-l border-gray-400 px-2 text-[13px] justify-center transition-all">
+                <span className="font-semibold">Model Bisnis</span>
+                <span>Sesuaikan</span>
+            </div>
+            <div className="hover:bg-slate-100 cursor-pointer w-full max-w-[150px] flex flex-col border-l border-gray-400 px-2 text-[13px] justify-center transition-all">
+                <span className="font-semibold">Harga</span>
+                <span>Sesuaikan</span>
+            </div>
+            <div className="flex justify-center items-center h-full w-[42px] bg-white rounded-full ml-auto cursor-pointer hover:bg-[#a5408c] hover:text-white transition-all">
+                <Search />
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar
