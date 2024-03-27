57d883b2a1f1758544a6e7703f0cc18f2d1172d5
cihuyyama <miqbalalhabib@gmail.com>
2024-03-27 23:17:44 +0700

    feat: add shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,47 +1,76 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'inter': ["Inter", "sans-serif"],
-      },
-      animation: {
-        'text-slide': 'text-slide 12.5s cubic-bezier(0.83, 0, 0.17, 1) infinite',
-      },
-      keyframes: {
-        'text-slide': {
-          '0%, 12.5%': {
-            transform: 'translateY(0%)',
-          },
-          '14.28%, 25.04%': {
-            transform: 'translateY(-12.5%)',
-          },
-          '28.56%, 37.5%': {
-            transform: 'translateY(-25.04%)',
-          },
-          '42.84%, 50.04%': {
-            transform: 'translateY(-37.5%)',
-          },
-          '53.56%, 62.5%': {
-            transform: 'translateY(-50.04%)',
-          },
-          '67.84%, 75.04%': {
-            transform: 'translateY(-62.5%)',
-          },
-          '78.56%, 87.5%': {
-            transform: 'translateY(-75.04%)',
-          },
-          '92.84%, 100%': {
-            transform: 'translateY(-87.5%)',
-          },
-        },
-      },
+export const darkMode = ["class"];
+export const content = [
+  './pages/**/*.{ts,tsx}',
+  './components/**/*.{ts,tsx}',
+  './app/**/*.{ts,tsx}',
+  './src/**/*.{ts,tsx}',
+];
+export const prefix = "";
+export const theme = {
+  container: {
+    center: true,
+    padding: "2rem",
+    screens: {
+      "2xl": "1400px",
     },
   },
-  plugins: [],
-}
-
+  extend: {
+    colors: {
+      border: "hsl(var(--border))",
+      input: "hsl(var(--input))",
+      ring: "hsl(var(--ring))",
+      background: "hsl(var(--background))",
+      foreground: "hsl(var(--foreground))",
+      primary: {
+        DEFAULT: "hsl(var(--primary))",
+        foreground: "hsl(var(--primary-foreground))",
+      },
+      secondary: {
+        DEFAULT: "hsl(var(--secondary))",
+        foreground: "hsl(var(--secondary-foreground))",
+      },
+      destructive: {
+        DEFAULT: "hsl(var(--destructive))",
+        foreground: "hsl(var(--destructive-foreground))",
+      },
+      muted: {
+        DEFAULT: "hsl(var(--muted))",
+        foreground: "hsl(var(--muted-foreground))",
+      },
+      accent: {
+        DEFAULT: "hsl(var(--accent))",
+        foreground: "hsl(var(--accent-foreground))",
+      },
+      popover: {
+        DEFAULT: "hsl(var(--popover))",
+        foreground: "hsl(var(--popover-foreground))",
+      },
+      card: {
+        DEFAULT: "hsl(var(--card))",
+        foreground: "hsl(var(--card-foreground))",
+      },
+    },
+    borderRadius: {
+      lg: "var(--radius)",
+      md: "calc(var(--radius) - 2px)",
+      sm: "calc(var(--radius) - 4px)",
+    },
+    keyframes: {
+      "accordion-down": {
+        from: { height: "0" },
+        to: { height: "var(--radix-accordion-content-height)" },
+      },
+      "accordion-up": {
+        from: { height: "var(--radix-accordion-content-height)" },
+        to: { height: "0" },
+      },
+    },
+    animation: {
+      "accordion-down": "accordion-down 0.2s ease-out",
+      "accordion-up": "accordion-up 0.2s ease-out",
+    },
+  },
+};
+// eslint-disable-next-line no-undef
+export const plugins = [require("tailwindcss-animate")];
