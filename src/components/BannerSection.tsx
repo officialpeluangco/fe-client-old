4b78955017d8daea078556513bdeca30bac2cf7e
cihuyyama <miqbalalhabib@gmail.com>
2024-06-14 20:16:15 +0700

    feat: add two banner

--- src/components/BannerSection.tsx
+++ src/components/BannerSection.tsx
@@ -4,19 +4,43 @@ import {
     CarouselItem,
 } from "@/components/ui/carousel"
 import { AspectRatio } from "@/components/ui/aspect-ratio"
+import Autoplay from "embla-carousel-autoplay"
+import { useRef } from "react"
 
 const BannerSection = () => {
+    const plugin = useRef(
+        Autoplay({ delay: 5000, stopOnInteraction: false })
+    )
 
 
     return (
         <div className="w-full">
             <AspectRatio ratio={1190 / 250} className='w-full max-w-[1190px] flex justify-center items-center mx-auto px-3 md:px-0 mt-4 md:mt-0'>
                 <div className='w-full h-full flex justify-center items-center'>
-                    <Carousel>
+                    <Carousel
+                        opts={{
+                            loop: true
+                        }}
+                        plugins={[plugin.current]}
+                    >
                         <CarouselContent>
                             <CarouselItem>
                                 <img
-                                    src="https://peluang-images.s3.ap-southeast-1.amazonaws.com/MISNOW/Banner+Missnow.png"
+                                    src="https://peluang-images.s3.ap-southeast-1.amazonaws.com/banner/Banner+Missnow.png"
+                                    alt="banner"
+                                    className='w-full h-full object-cover rounded-lg border-2 border-slate-100'
+                                />
+                            </CarouselItem>
+                            <CarouselItem>
+                                <img
+                                    src="https://peluang-images.s3.ap-southeast-1.amazonaws.com/banner/banner-kebuli-abuya.png"
+                                    alt="banner"
+                                    className='w-full h-full object-cover rounded-lg border-2 border-slate-100'
+                                />
+                            </CarouselItem>
+                            <CarouselItem>
+                                <img
+                                    src="https://peluang-images.s3.ap-southeast-1.amazonaws.com/banner/banner-balkan-shawarma.png"
                                     alt="banner"
                                     className='w-full h-full object-cover rounded-lg border-2 border-slate-100'
                                 />
